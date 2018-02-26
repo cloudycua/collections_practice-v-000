ccbeb486660afaad17f306b218d48a6ea484bac3
Christine Cua <cloudycua@gmail.com>
2018-02-26 22:25:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,8 +18,8 @@ def swap_elements(array)
 #  array[1] = old_third
 #  array[2] = old_second
 #  array
-array[1], array[2] = array[2], array[1]
-array
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
